c622df8689330c6dda2e8072b97ce0ec00d7e1d7
timerix <timerix11@gmail.com>
2023-02-11 14:49:04 +0600

    c# solution added

---
+++ c#/ChatAPI/Client.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ChatAPI
+{
+    public class ServerInfo
+    {
+        public string name;
+        public string? ip;
+        public string? domain;
+        public int port;
+        public string publicKey;
+        public bool isIP;
+
+        public ServerInfo(string ipOrDomain, bool isIP, int port, string name, string publicKey)
+        {
+            this.isIP = isIP;
+            if (isIP) ip = ipOrDomain;
+            else domain = ipOrDomain;
+            this.port = port;
+            this.name = name;
+            this.publicKey = publicKey;
+        }
+    }
+    
+    public class Client
+    {
+        private Socket? serverConnection;
+        
+        public void ConnectToServer(ServerInfo serverInfo)
+        {
+            if (serverConnection is not null && serverConnection.Connected)
+            {
+                serverConnection.Close();
+            }
+            serverConnection = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPAddress serverIP;
+            if (serverInfo.isIP)
+                serverIP = IPAddress.Parse(serverInfo.ip!);
+            else serverIP = Dns.GetHostAddresses(serverInfo.domain!)[0];
+            IPEndPoint serverEndpoint=new IPEndPoint(serverIP, serverInfo.port);
+            serverConnection.Connect(serverEndpoint);
+        }
+
+        public void UpdateOnlineStatus(bool isOnline)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ClientConnectionInfo[] GetOnlineClients()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ConnectToClient(ClientConnectionInfo clientInfo)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class ClientConnectionInfo
+    {
+        public string publicKey;
+        public string[] encriprionAlgorithms;
+        public string[] hashAlgorithms;
+
+        public ClientConnectionInfo(string publicKey, string[] encriprionAlgorithms, string[] hashAlgorithms)
+        {
+            this.publicKey = publicKey;
+            this.encriprionAlgorithms = encriprionAlgorithms;
+            this.hashAlgorithms = hashAlgorithms;
+        }
+    }
+}
---
+++ c#/ChatServer/Program.cs
@@ -0,0 +1,28 @@
+﻿using ChatAPI;
+using System.Net;
+using System.Net.Sockets;
+
+var config = new ServerConfig("172.16.0.210", 100);
+Socket listeningSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+listeningSocket.Bind(
+    new IPEndPoint(IPAddress.Parse(config.localAddress), 
+        config.port));
+listeningSocket.Listen();
+
+while (true)
+{
+    var clientSocket = listeningSocket.Accept();
+    Console.WriteLine("client connected");
+}
+
+public class ServerConfig
+{
+    public string localAddress;
+    public int port;
+
+    public ServerConfig(string localAddress, int port)
+    {
+        this.port = port;
+        this.localAddress = localAddress;
+    }
+}
